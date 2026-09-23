d6eb0c1e8adc9e07936dfd1cf3decadef07f5440
polina301 <polyaa99b@gmail.com>
2026-09-24 01:07:41 +0300

    Hi top-it

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 int main()
-{ std::cout << "Hello, top-it!" <<"\n"; 
+{ std::cout << "Hi, top-it!" <<"\n"; 
  std::cout << "   ***   ***   \n";
  std::cout << "*      *      *\n";
  std::cout << "  *         *\n";
