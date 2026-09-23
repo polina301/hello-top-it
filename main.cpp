5bfaba86a105b41d1867a59cceb6a424792e0d45
polina301 <polyaa99b@gmail.com>
2026-09-24 00:53:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
 #include <iostream>
 int main()
-{ std::cout << "Hello, top-it!" <<"\n"; }
+{ std::cout << "Hello, top-it!" <<"\n"; 
+ std::cout << "   ***   ***   \n";
+ std::cout << "*      *      *\n";
+ std::cout << "  *         *\n";
+ std::cout << "       *\n";
+}
